f2d240774b8667219822665ff80c179739286e04
samtomcom <sam.comer@gmail.com>
2020-11-08 14:17:40 +0000

    README, made script more customisable

---
+++ README.md
@@ -0,0 +1,25 @@
+# \*arr Post Process
+
+A sample post process script for Sonarr/Radarr.  
+Currently for Windows, plan to make it system independant eventually.
+
+## Usage
+
+*arr Post Process is a script that simplifies the interface for getting environment variables from *arr. *arr Post Process simply creates a dictionary of the enviroment variables that *arr returns, the full list of which can be found [here (Sonarr)](https://github.com/Sonarr/Sonarr/wiki/Custom-Post-Processing-Scripts) and [here (Radarr)](https://github.com/Radarr/Radarr/wiki/Custom-Post-Processing-Scripts).
+
+If you want to extend this to some other *arr/etc software, extend the list of environment variables, stored in ARR_ENV.txt
+
+## Requirements
+
+* Sonarr/Radarr installed
+* Python 3.6+ installed and added to your system PATH
+
+## Installation
+
+1. Clone the repository
+
+    git clone https://git.github.com/samtomcom/arr_post_process.git
+
+2. Edit `execute.bat` to the correct location.
+3. Naviage to Sonarr/Radarr [Settings > Connect > + > Custom Script](https://i.imgur.com/UOhYbNf.png), fill in the Path variable to `your\install\path\execute.bat`
+4. Test and save
--- arr_post_process.py
+++ arr_post_process.py
@@ -1,26 +1,30 @@
-from datetime import datetime
 import os
 
 SCRIPT_PATH = os.path.realpath(__file__)
 SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
-TIME = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-LOG_FILE = f"{SCRIPT_DIR}\\logs\\{TIME}.txt"
 ENV_FILE = f"{SCRIPT_DIR}\\ARR_ENV.txt"
 
 envs = {}
 
+# Checking all potentiall environment variables
 with open(ENV_FILE, 'r') as f:
     for line in f:
         env = line.rstrip()
         if env in os.environ:
             envs[env] = os.environ[env]
 
+    # No environment variables were found
+    # Either the script was run manually as a test (ie Not ran by *arr)
+    #  or, something went wrong : )
     if len(envs) == 0:
-        envs['TEST'] = 'This was a test'
+        raise Exception('No *arr Environment variables were found.')
 
-with open(LOG_FILE, 'w') as f:
-    for env in envs:
-        f.write(f'{env} = {envs[env]}\n')
+# envs is now a dictionary containing any *arr variables found
+# Call your own script here, or do what you want with the environment variables found
+# Information on what these variables could be can be found at:
+# https://github.com/Radarr/Radarr/wiki/Custom-Post-Processing-Scripts
+# https://github.com/Sonarr/Sonarr/wiki/Custom-Post-Processing-Scripts
+
 	
 
 
